86ef92a1896b8bf7cf0700a2173a416f50dcef2d
15101990 <m.helmy.amit@gmail.com>
2021-10-29 15:20:55 +0200

    ghghgh

---
+++ ahmed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    int i;
+    for(i=x;i!=0;i--)
+    {
+    // printf("Hello world!\n");
+    }
+    return 0;
+}
